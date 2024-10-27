2c23482bcd4e8d4e74988d04ca68e1bc462a9e26
eumonti <eugeniomontinaro@gmail.com>
2024-10-27 16:17:02 +0100

    update .gitignore

--- include/sinric_secrets.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define APP_KEY "APP_KEY"
-#define APP_SECRET "APP_SECRET"
-#define LIGHT_ID "LIGHT_ID"
--- include/wifi_credentials.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define WIFI_SSID "WIFI_SSID"
-#define WIFI_PASS "WIFI_PASS"
